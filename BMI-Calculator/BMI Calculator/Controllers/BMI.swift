6e46fe30a840c88e33327db3fc111adf503f684d
Ilya Popov <71981296+poposhka1990@users.noreply.github.com>
2023-01-05 17:24:08 +0300

    Add files via upload

---
+++ BMI-Calculator/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Илья Попов on 05.01.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
---
+++ BMI-Calculator/BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Илья Попов on 04.01.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: .systemBlue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: .green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies...", color: .red)
+        }
+    }
+    
+    func getBMIValue() -> String {
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
+        return bmiTo1DecimalPlace
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "Error"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .black
+    }
+}
