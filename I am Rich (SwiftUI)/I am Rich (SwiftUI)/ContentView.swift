50eae3f59caead25d6375bce22ce7736b1bfcb12
Ilya Popov <71981296+poposhka1990@users.noreply.github.com>
2023-01-28 19:27:50 +0300

    Add files via upload

---
+++ I am Rich (SwiftUI)/I am Rich (SwiftUI)/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  I am Rich (SwiftUI)
+//
+//  Created by Илья Попов on 28.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 250, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I am Rich (SwiftUI)/I am Rich (SwiftUI)/I_am_Rich__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich__SwiftUI_App.swift
+//  I am Rich (SwiftUI)
+//
+//  Created by Илья Попов on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Rich__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
