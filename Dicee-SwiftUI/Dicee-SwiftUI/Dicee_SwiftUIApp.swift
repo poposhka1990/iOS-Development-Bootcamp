7352c3e7de11cfd354443cdc2ebcbd2218e8538f
Ilya Popov <71981296+poposhka1990@users.noreply.github.com>
2023-01-29 21:05:03 +0300

    Add files via upload

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Илья Попов on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
